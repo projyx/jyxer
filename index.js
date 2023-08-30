8421282b5d4959d7ea72827e22e4b9d39fa28f9a
Izm <123246207+thebanon@users.noreply.github.com>
2023-08-30 14:11:32 -0400

    Update /

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ window.onload = async(event)=>{
     console.log(2, "window.onload", {
         manifest: window.manifest
     });
-    browse.route(window.location.pathname);
+    //browse.route(window.location.pathname);
 }

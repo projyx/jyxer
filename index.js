8ccf0495f350353b91ba0f4c3bbd289b99ada05b
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2023-09-06 13:57:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 window.onload = async(event)=>{
     window.manifest = await request("/site.webmanifest");
     console.log(2, "window.onload", {
-        manifest: window.manifest
+        browse: window.browse,
+        manifest: window.manifest,
+        request: window.request
     });
     browse.route(window.location.pathname);
 }

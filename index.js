a01cbe196fe8c05b6b2ad39a91040c1153f24668
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2023-12-28 15:54:31 -0500

    Update Frontend
    Update Home UI
    Create Apps JSON File
    Create Theme Color
    Create PHO JS API

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ window.onload = async(event)=>{
     });
     browse.route(window.location.pathname);
 }
+
+window.PHO = {}
+window.PHO.ton = {}
+window.PHO.ton.icE = ()=>{
+    var body = event.target.closest('body');
+    body.classList.remove('heads');
+    body.classList.remove('tails');
+    if (flipResult <= 0.5) {
+        body.classList.add('heads');
+        body.classList.remove('tails');
+        console.log('it is head');
+    } else {
+        coin.classList.remove('heads');
+        coin.classList.add('tails');
+        console.log('it is tails');
+    }
+    var coin = event.target.closest('#coin');
+    coin.classList.remove('heads');
+    coin.classList.remove('tails');
+    if (flipResult <= 0.5) {
+        coin.classList.add('heads');
+        coin.classList.remove('tails');
+        console.log('it is head');
+    } else {
+        coin.classList.remove('heads');
+        coin.classList.add('tails');
+        console.log('it is tails');
+    }
+}

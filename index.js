a0179d4f2e3def10a2f3d6b682a6ae84c64d8d05
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2024-01-17 08:28:58 -0500

    Update Frontend

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onload = async(event)=>{
-    window.manifest = await request("/site.webmanifest");
+    window.manifest = await request("site.webmanifest");
     console.log(2, "window.onload", {
         browse: window.browse,
         manifest: window.manifest,
         request: window.request
     });
-    browse.route(window.location.pathname);
+    rout.er(window.location.pathname);
 }
 
 window.PHO = {}
